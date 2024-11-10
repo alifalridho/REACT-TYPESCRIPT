f37f40400fc7e31eee002ca0e7d25a08f782e1e5
alifalridho <alifdanyusuf@gmail.com>
2024-11-10 23:20:47 +0700

    add PersonList.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,25 @@ import Greet from "./components/Greet";
 import Hello from "./components/Hello";
 import User from "./components/User";
 import Person from "./components/Person";
+import PersonList from "./components/PersonList";
 
 function App() {
     const personName = {
         firstName: "Bruce",
         lastName: "Wayne",
     };
+    const personList = [
+        { first: "Bruce", last: "Wayne" },
+        { first: "Clark", last: "Kane" },
+        { first: "Joker", last: "Junior" },
+    ];
     return (
         <div className="App">
             <Greet name={"Alif"} />
             <Hello age={25} name="Alif" />
             <User name="Efraim" age={24} status={"taken"} isLoggedin={true} />
             <Person name={personName} />
+            <PersonList names={personList} />
         </div>
     );
 }
---
+++ src/components/PersonList.tsx
@@ -0,0 +1,17 @@
+type PersonListProps = {
+    names: { first: string; last: string }[];
+};
+
+const PersonList = ({ names }: PersonListProps) => {
+    return (
+        <div>
+            {names.map((name) => (
+                <h2>
+                    {name.first} {name.last}
+                </h2>
+            ))}
+        </div>
+    );
+};
+
+export default PersonList;
